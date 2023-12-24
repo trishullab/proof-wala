829ad017ead9dfb7099b9602c3b3b335d7e07eab
Amitayush Thakur <amitayush@utexas.edu>
2023-12-24 20:52:49 +0000

    Updated the itp-interface version

--- src/data_gen/data_gen.py
+++ src/data_gen/data_gen.py
@@ -1,4 +1,8 @@
-from itp_interface.main.run_tool import main
+#from itp_interface.main.run_tool import main
+from itp_interface.tools.training_data import TrainingData
 
 if __name__ == '__main__':
-    main()
+    td = TrainingData(folder='/home/amthakur/Projects/thrall/.log/train', training_meta_filename='local.meta.json')
+    td.load()
+    data = [x for x in td]
+    print(len(data))
