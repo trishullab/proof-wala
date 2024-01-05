eddd60a3dc2736480549bf83529af87a73e6b589
Amitayush Thakur <amitayush@utexas.edu>
2024-01-05 04:56:13 +0000

    Renamed the thrall to thrall_lib

---
+++ src/thrall_lib/main/run_training.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+root_dir = f"{__file__.split('thrall_lib')[0]}"
+if root_dir not in sys.path:
+    sys.path.append(root_dir)
+from thrall_lib.llm_helpers.model import Model
