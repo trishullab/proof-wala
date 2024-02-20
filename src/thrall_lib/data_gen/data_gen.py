4991d8576f18c3d8a9fe3abf80ac376e4b435384
Amitayush Thakur <amitayush@utexas.edu>
2024-02-20 02:18:53 +0000

    Removed unused data_gen folder

--- src/thrall_lib/data_gen/data_gen.py
+++
@@ -1,8 +0,0 @@
-#from itp_interface.main.run_tool import main
-from itp_interface.tools.training_data import TrainingData
-
-if __name__ == '__main__':
-    td = TrainingData(folder='/mnt/sdd1/amthakur/data/random_test', training_meta_filename='local.meta.json')
-    td.load()
-    data = [x for x in td]
-    print(len(data))
