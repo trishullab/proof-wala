95412343202b0d35600a3b9ab46451e731b76c92
Amitayush Thakur <amitayush@utexas.edu>
2024-05-26 00:18:44 +0000

    change python3 to python in update_imports.sh

--- src/thrall_lib/scripts/update_imports.sh
+++ src/thrall_lib/scripts/update_imports.sh
@@ -2,8 +2,8 @@
 
 pushd imports/itp-interface
 git submodule update --init --recursive
-python3 -m pip install --upgrade build
-python3 -m build
+python -m pip install --upgrade build
+python -m build
 popd
 pip install imports/itp-interface/dist/itp_interface-0.1.5-py3-none-any.whl --force-reinstall
 install-itp-interface
